7f544150e21ab7aa1121fe2178fe49ff6db3f7ce
Emerson Pedroso <emerson@bitcoinnano.org>
2024-01-28 15:42:52 -0300

    fix

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,59 +1,36 @@
 import './player.css';
 
-
 import {
   MediaPlayer,
   MediaProvider,
   Poster,
-  isYouTubeProvider,
-  type MediaProviderAdapter
-  
-
 } from '@vidstack/react';
 import {
   defaultLayoutIcons,
   DefaultVideoLayout,
-  
 } from '@vidstack/react/player/layouts/default';
 
-
-
 export function Player() {
-  function onProviderChange(provider: MediaProviderAdapter | null) {
-    if (isYouTubeProvider(provider)) {
-      provider.cookies = true;
-    }
-  }
-
   return (
-    
     <>
-    
       <MediaPlayer
-        autoplay
+        // autoplay
         load="visible"
         className="player"
-        title="Sprite Fight"
-        src={"https://youtu.be/iNsiQHRw2iE?si=Gtvkx4nrcChcNS6y"}
+        title="Ecos da Identidade."
+        src="https://www.youtube.com/embed/bUqi5kupCNU" // Substitua '_cMxraX_5RE' pelo ID do vídeo do YouTube
         aspectRatio="16/9"
         crossorigin
-      
       >
         <MediaProvider>
           <Poster
             className="vds-poster"
-            src="https://crieseudestino.com/wp-content/uploads/elementor/thumbs/o-que-é-pnl-1080-nyhzbfmlas0vzsdl7yblmtffx02y7ue8vkhcfhcdio.jpg"
+            src="play.gif"
             alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
           />
-         
         </MediaProvider>
-
-        
-        <DefaultVideoLayout
-          icons={defaultLayoutIcons}
-        />
+        <DefaultVideoLayout icons={defaultLayoutIcons} />
       </MediaPlayer>
-
     </>
   );
 }

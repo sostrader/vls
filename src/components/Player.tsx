49fafd72419521768a844c7849dff778bcd87148
Emerson Pedroso <emerson@bitcoinnano.org>
2024-02-02 15:05:36 -0300

    test

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -4,8 +4,9 @@ import './player.css';
 
 
 import { useState } from 'react';
-import { MediaPlayer, MediaProvider, Poster, isYouTubeProvider,
+import { MediaPlayer, MediaProvider, Poster, isHLSProvider,
   type MediaProviderAdapter,
+  type MediaProviderChangeEvent,
 } from '@vidstack/react';
 import { defaultLayoutIcons, DefaultAudioLayout, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
 import ProgressBar from './ProgressBar';
@@ -13,8 +14,17 @@ import DelayButton from './DelayedButton'
 
 
 export function Player() {
-
-
+  function onProviderChange(
+    provider: MediaProviderAdapter | null,
+    nativeEvent: MediaProviderChangeEvent,
+  ) {
+    if (isHLSProvider(provider)) {
+      // Default development URL.
+      provider.library = 'https://cdn.jsdelivr.net/npm/hls.js@^1.0.0/dist/hls.js';
+      // Default production URL.
+      provider.library = 'https://cdn.jsdelivr.net/npm/hls.js@^1.0.0/dist/hls.min.js';
+    }
+  }
   
   const [isPlaying, setIsPlaying] = useState(false);
 
@@ -26,18 +36,20 @@ export function Player() {
     <div className="player-container">
       <div className="video-and-progress-container">
         <MediaPlayer
+        onProviderChange={onProviderChange}
+          autoplay
           playsinline
           aspectRatio="16/9"
           load="visible"
           className="player" // Garante que o player utilize a classe correta para responsividade
           title="Ecos da Identidade."
-          src="https://player.vimeo.com/video/909150840?h=f98a95c701"
+          src="/video/play.m3u8"
           crossorigin
           onPlay={handlePlay}
           onPause={handlePause}
         >
           <MediaProvider>
-            <Poster className="vds-poster" src="https://vls-zeta.vercel.app/clicktoplay.gif" alt="Play!" />
+            <Poster className="vds-poster" src="cover.png" alt="Play!" />
           </MediaProvider>
           <DefaultAudioLayout icons={defaultLayoutIcons} />
           <DefaultVideoLayout icons={defaultLayoutIcons} />

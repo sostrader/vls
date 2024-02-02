08ac82aaa3c59e55f753ea80a23fcfe5e9620f71
Emerson Pedroso <emerson@bitcoinnano.org>
2024-02-02 16:48:56 -0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-950 text-white">
       {/* <Header title1="A Mais Abrangente Exploração Científica das Personalidades Já Realizada" title2="Método Único no Mundo" title3="Ecos da Identidade" /> */}
-      <Header title1="Descubra o Poder Único da Classificação de Personalidades por Data de Nascimento" title2="O Primeiro e Mais Abrangente Método no Mundo"/>
+      <Header title1="Descubra o Poder Único da Classificação de Personalidades por Data de Nascimento" title2="O Primeiro e Mais Abrangente Método Científico no Mundo"/>
       <main className="flex flex-col items-center p-0 w-full">
       <Player />
       </main>

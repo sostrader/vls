0db72d2525e4fa98de1e8f778329f719e854a055
Emerson Pedroso <emerson@bitcoinnano.org>
2024-02-02 15:12:25 -0300

    fix

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -4,9 +4,8 @@ import './player.css';
 
 
 import { useState } from 'react';
-import { MediaPlayer, MediaProvider, Poster, isHLSProvider,
+import { MediaPlayer, MediaProvider, Poster, isYouTubeProvider,
   type MediaProviderAdapter,
-  type MediaProviderChangeEvent,
 } from '@vidstack/react';
 import { defaultLayoutIcons, DefaultAudioLayout, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
 import ProgressBar from './ProgressBar';
@@ -14,17 +13,11 @@ import DelayButton from './DelayedButton'
 
 
 export function Player() {
-  function onProviderChange(
-    provider: MediaProviderAdapter | null,
-    nativeEvent: MediaProviderChangeEvent,
-  ) {
-    if (isHLSProvider(provider)) {
-      // Default development URL.
-      provider.library = 'https://cdn.jsdelivr.net/npm/hls.js@^1.0.0/dist/hls.js';
-      // Default production URL.
-      provider.library = 'https://cdn.jsdelivr.net/npm/hls.js@^1.0.0/dist/hls.min.js';
+    function onProviderChange(provider: MediaProviderAdapter | null) {
+      if (isYouTubeProvider(provider)) {
+        provider.cookies = true;
+      }
     }
-  }
   
   const [isPlaying, setIsPlaying] = useState(false);
 
@@ -43,13 +36,13 @@ export function Player() {
           load="visible"
           className="player" // Garante que o player utilize a classe correta para responsividade
           title="Ecos da Identidade."
-          src="/video/play.m3u8"
+          src="https://www.youtube-nocookie.com/embed/4tUU66blhuY"
           crossorigin
           onPlay={handlePlay}
           onPause={handlePause}
         >
           <MediaProvider>
-            <Poster className="vds-poster" src="cover.png" alt="Play!" />
+            <Poster className="vds-poster" src="https://vls-zeta.vercel.app/clicktoplay.gif" alt="Play!" />
           </MediaProvider>
           <DefaultAudioLayout icons={defaultLayoutIcons} />
           <DefaultVideoLayout icons={defaultLayoutIcons} />

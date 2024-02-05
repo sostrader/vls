94ae7c1534727d8ca686a00f0f2ca2d40a51e64f
Emerson Pedroso <emerson@bitcoinnano.org>
2024-02-05 15:55:40 -0300

    FIX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-950 text-white">
       {/* <Header title1="A Mais Abrangente Exploração Científica das Personalidades Já Realizada" title2="Método Único no Mundo" title3="Ecos da Identidade" /> */}
-      <Header title1="DESCUBRA O PODER ÚNICO DA CLASSIFICAÇÃO DE PERSONALIDADES POR DATA DE NASCIMENTO O PRIMEIRO E MAIS ABRANGENTE MÉTODO CIENTÍFICO NO MUNDO" title2=""/>
+      <Header title1="MÉTODO ÚNICO E CIENTÍFICO DE INSIGHTS DA PERSONALIDADE" title2="POR DATA DE NASCIMENTO"/>
       <main className="flex flex-col items-center p-0 w-full">
       <Player />
       </main>
--- src/app/psc/10/page.tsx
+++ src/app/psc/10/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-950 text-white">
       {/* <Header title1="A Mais Abrangente Exploração Científica das Personalidades Já Realizada" title2="Método Único no Mundo" title3="Ecos da Identidade" /> */}
-      <Header title1="O MÉTODO CIENTÍFICO ÚNICO DE INSIGHTS DA PERSONALIDADE" title2="PELA DATA DE NASCIMENTO"/>
+      <Header title1="MÉTODO ÚNICO E CIENTÍFICO DE INSIGHTS DA PERSONALIDADE" title2="PELA DATA DE NASCIMENTO"/>
       <main className="flex flex-col items-center p-0 w-full">
       <Player />
       </main>
